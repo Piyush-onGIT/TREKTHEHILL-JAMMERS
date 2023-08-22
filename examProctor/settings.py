8f36b4e15fd1b30865f70324d85019ea5cab464d
Piyush-onGIT <thisissahupiyush@gmail.com>
2023-08-22 12:57:03 +0530

    changed requirements.txt

--- examProctor/settings.py
+++ examProctor/settings.py
@@ -24,14 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eabmzp&9s&35%lg4mrace#$bn&u(w&hin&ssf0u@$$fi**#gyz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1', 'examproctor.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'examproctor.herokuapp.com', '*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'exam.apps.ExamConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
