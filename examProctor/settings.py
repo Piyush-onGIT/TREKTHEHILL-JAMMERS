6cfd92b5178c2c6d3ab99c1941cde5f05ae9b045
Piyush-onGIT <thisissahupiyush@gmail.com>
2023-08-22 13:06:14 +0530

    debug true

--- examProctor/settings.py
+++ examProctor/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eabmzp&9s&35%lg4mrace#$bn&u(w&hin&ssf0u@$$fi**#gyz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1', 'examproctor.herokuapp.com', '*']
 
